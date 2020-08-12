2b8f7d0c2043f6c1534bbc56f3b982c69e11c22d
Karthik-B-06 <karthik06051996@gmail.com>
2020-08-12 13:57:39 +0530

    chore: :zap: Added an alias for custom tailwind config styles imports

--- App.js
+++ App.js
@@ -9,64 +9,26 @@
 import React from 'react';
 import {
   SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
+  StatusBar, StyleSheet,
+  Text, View
 } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { tailwind } from 'tailwind';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
+
+const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+        <View style={tailwind('pt-12 items-center')}>
+          <View style={tailwind('bg-blue-200 px-3 py-1 rounded-full')}>
+            <Text style={tailwind('text-blue-800 font-semibold')}>
+              Hello Tailwind !
+				    </Text>
           </View>
-        </ScrollView>
+        </View>
       </SafeAreaView>
     </>
   );
