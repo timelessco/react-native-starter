2615d8b76f833b20481749dd5ea4bcc2808f7687
Karthik-B-06 <karthik06051996@gmail.com>
2020-08-13 12:30:15 +0530

    fix: :twisted_rightwards_arrows: Merged .eslintrc into .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,18 @@
 module.exports = {
   root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
   env: {
     es6: true,
     node: true,
     jest: true,
+    browser: true,
+    'react-native/react-native': true,
   },
   extends: [
+    'react-native',
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/eslint-recommended',
@@ -13,11 +20,17 @@ module.exports = {
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'prettier/@typescript-eslint',
   ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: './tsconfig.json',
+  ecmaFeatures: {
+    jsx: true,
+    classes: true,
   },
-  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+  plugins: [
+    'react',
+    'react-hooks',
+    '@typescript-eslint',
+    'prettier',
+    'react-native',
+  ],
   rules: {
     quotes: ['error', 'single'],
     'prettier/prettier': 'error',
