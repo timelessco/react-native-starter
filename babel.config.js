68672c3c0c086c890b0713a60d1fcdf97d5d4634
criston <criston@timeless.co>
2022-10-04 15:44:15 +0530

    feat: 🚀 Upgrade & change dependencies to latest version

--- babel.config.js
+++ babel.config.js
@@ -1,22 +1,4 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        extensions: [
-          '.ios.js',
-          '.android.js',
-          '.js',
-          '.ts',
-          '.tsx',
-          '.json',
-        ],
-        alias: {
-          tailwind: './tailwind/',
-        },
-        cwd: 'babelrc',
-      },
-    ],
-  ],
+  plugins: ['nativewind/babel', 'react-native-reanimated/plugin'],
 };
--- src/app-root/index.tsx
+++ src/app-root/index.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { StatusBar, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { tailwind } from 'tailwind';
 
 const AppRoot: React.FC = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <NavigationContainer>
-        <View style={tailwind('flex-1 justify-center items-center')}>
+        <View className="flex-1 justify-center items-center">
           <Text>Hello, World</Text>
         </View>
       </NavigationContainer>
